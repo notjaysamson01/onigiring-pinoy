6082a39f669cf255f7ac1e66447adf857b5e2f9c
notjaysamson01 <pogisijayjay12@gmail.com>
2026-02-22 23:31:58 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -35,3 +35,33 @@ document.addEventListener("DOMContentLoaded", () => {
         setTimeout(() => el.classList.add("show"), i * 150)
     );
 });
+
+function openModal() {
+    document.getElementById("orderModal").style.display = "block";
+}
+
+function closeModal() {
+    document.getElementById("orderModal").style.display = "none";
+}
+
+// Close when clicking outside modal
+window.onclick = function(event) {
+    const modal = document.getElementById("orderModal");
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+
+document.getElementById("orderForm").addEventListener("submit", function(e) {
+
+    const tinapang = parseInt(document.querySelector('[name="tinapang_qty"]').value) || 0;
+    const adobo = parseInt(document.querySelector('[name="adobo_qty"]').value) || 0;
+    const longganisa = parseInt(document.querySelector('[name="longganisa_qty"]').value) || 0;
+
+    if (tinapang === 0 && adobo === 0 && longganisa === 0) {
+        alert("Please order at least one item.");
+        e.preventDefault();
+        alert("dumbass");
+        e.preventDefault();
+    }
+});
